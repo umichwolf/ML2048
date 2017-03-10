2f4afd28a2b96325242d3289ff0dd2a8fe72d187
Yitong Sun <syitong@umich.edu>
2017-03-09 23:08:02 -0500

    so far the best parameter is recorded in the traintest.

--- traintest.py
+++ traintest.py
@@ -7,18 +7,16 @@ filename = "data_train.txt"
 data = Minput.read_training_data(filename)
 X = data["X"]
 Y = data["Y"]
-
+mgr = Msolver.Solver()
+X = mgr.cleanup(X)
 game_info = {"type":"2048",
              "size":4,
              "moves":["w","a","s","d"]}
 machine_info = {"type":"SVM",
-                "gamma":10**(-5),
+                "gamma":10**(-1.4),
                 "no_features":500,
-                "alpha":10**(-1.9)}
-
-mgr = Msolver.Solver()
-#mgr.load('SVM','train1')
+                "alpha":10**(-2.45)}
 mgr.new(game_info,machine_info,"better")
-data_list = mgr.machines[0]["machine"].cleanup(X)
-mgr.machines[0]["machine"].train(X,Y)
+score = mgr.machines[0]["machine"].train(X,Y)
+print score
 mgr.save(0)
