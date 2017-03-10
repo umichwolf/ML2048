ed42046780e2a6fdca07e614510a4d7fc46d6ecd
Yitong Sun <syitong@umich.edu>
2017-03-09 21:46:59 -0500

    tuning the parameter of SVM to achieve 37% score on data_train.txt

--- traintest.py
+++ traintest.py
@@ -1,52 +1,24 @@
 from solver import Msolver
 import numpy as np
+import Minput
 
 # Read training data from output file
-try:
-    f = open("data_train.txt","r")
-    dataset = list()
-    for line in f:
-        row = line.split()
-        if row != []:
-            dataset.append(row)
-finally:
-    f.close()
-
-data_list = list()
-move_list = list()
-for idx in range(len(dataset)):
-    if len(dataset[idx])==17:
-        move = dataset[idx].pop(16)
-    else:
-        move = "f"
-    if move in ["w","a","s","d"]:
-        move_list.append(move)
-        data_list.append(map(int,dataset[idx]))
+filename = "data_train.txt"
+data = Minput.read_training_data(filename)
+X = data["X"]
+Y = data["Y"]
 
 game_info = {"type":"2048",
              "size":4,
              "moves":["w","a","s","d"]}
 machine_info = {"type":"SVM",
-                "gamma":0.002,
-                "no_features":1000,
-                "alpha":10**(-20)}
+                "gamma":10**(-5),
+                "no_features":500,
+                "alpha":10**(-1.9)}
 
 mgr = Msolver.Solver()
 #mgr.load('SVM','train1')
 mgr.new(game_info,machine_info,"better")
-data_list = mgr.machines[0]["machine"].cleanup(data_list)
-mgr.machines[0]["machine"].train(data_list,move_list)
+data_list = mgr.machines[0]["machine"].cleanup(X)
+mgr.machines[0]["machine"].train(X,Y)
 mgr.save(0)
-
-### check classification accuracy of machine 
-#score = 0
-#for idx in range(len(move_list)):
-#    pre_list = mgr.machines[0]["machine"].test([data_list[idx,:]])
-#    move_value = 0
-#    for key,value in pre_list.items():
-#        if value > move_value:
-#            move = key
-#    print move,' ',move_list[idx]
-#    if move==move_list[idx]:
-#        score = score + 1
-#print score,' ',len(move_list)
